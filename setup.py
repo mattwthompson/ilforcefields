e684761bb9fa024577e60382204d6209e06cea73
mattwthompson <mattwthomp@gmail.com>
2017-03-11 14:46:13 -0600

    Initialization

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "il-forcefields",
+    version = "0.0.0",
+    author = "Matt Thompson",
+    author_email = "matt.thompson@vanderbilt.edu",
+    description = ("A collection of ionic liquid forcefields and builders"))
