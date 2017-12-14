01017d0fe411e1259ea71f01e3ea45c7a5a47382
matt.thompson@vanderbilt.edu <matt.thompson@vanderbilt.edu>
2017-12-14 08:31:57 -0600

    Add deprecation warning for `get_lopes`

--- ilforcefields/lib/get_lopes.py
+++ ilforcefields/lib/get_lopes.py
@@ -1,8 +1,14 @@
 from pkg_resources import resource_filename
+import warnings
 
 from foyer import Forcefield
 
+
 def get_lopes():
+    warnings.warn("This is deprecated, use "
+                  "ilforcefield.utils.utils.get_forcefield('lopes')",
+                  DeprecationWarning)
+
     LOPES = Forcefield(resource_filename('ilforcefields',
                                          'lopes/lopes.xml'))
     return LOPES
