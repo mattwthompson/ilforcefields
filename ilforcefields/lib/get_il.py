4587c20b2dd55aabd69f29dee10b1650306b7a23
matt.thompson@vanderbilt.edu <matt.thompson@vanderbilt.edu>
2017-12-14 09:01:05 -0600

    Deprecate GetIL because it shouldnt be a class

--- ilforcefields/lib/get_il.py
+++ ilforcefields/lib/get_il.py
@@ -1,10 +1,15 @@
 import os
+import warnings
 
 import mbuild as mb
 from pkg_resources import resource_filename
 
 class GetIL(mb.Compound):
     def __init__(self, il_name):
+        warnings.warn("This is deprecated, instead use "
+                      "ilforcefield.utils.utils.get_il",
+                      DeprecationWarning)
+
         cache_dir = resource_filename('ilforcefields', 'lib/un-typed')
         filename = '{}.mol2'.format(il_name)
         if any(file == filename for file in os.listdir(cache_dir)):
